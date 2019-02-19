7597430e2623931ec8c3bfd8194a341710440794
Emmi Linkola <emmi.linkola@wunderdog.fi>
2019-02-19 14:09:16 +0200

    Add sign up component

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import cx from './index.module.scss'
 
 const SignUp = props => (
-  <div />
+  <div className={ cx['sign-up'] }>
+    <h3>Open your own cookie jar</h3>
+  </div>
 )
 
 export default SignUp
